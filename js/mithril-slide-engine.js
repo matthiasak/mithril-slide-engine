933c937a15b390a3d503f2b55705ce42d3288f6a
Matthew Keas <matthiasak@gmail.com>
2015-09-06 03:11:55 +0200

    adding better default styles, slightly cleaner API

--- js/mithril-slide-engine.js
+++ js/mithril-slide-engine.js
@@ -11,24 +11,19 @@ const computable = fn => {
     }
 }
 
-const engine = () => {
+export default () => {
     const parseHash = () => parseInt(window.location.hash.slice(1)) || 0
 
     const slides = m.prop([]),
         active = m.prop(parseHash()),
         prev = m.prop()
 
-    const insert = computable((val, index) => {
-        if(val instanceof Function) val = {view: val}
-        if(!val.controller) val.controller = () => {}
-
-        if(typeof index === 'undefined')
-            index = slides().length
-
-        const i = slides(),
-            [first, third] = [i.slice(0,index), i.slice(index+1)]
-
-        return slides([...first, val, ...third])
+    const insert = computable((..._slides) => {
+        return slides(_slides.map(val => {
+            if(val instanceof Function) val = {view: val}
+            if(!val.controller) val.controller = () => {}
+            return val
+        }))
     })
 
     const remove = computable(index => {
@@ -37,44 +32,96 @@ const engine = () => {
         return slides([...first, ...second])
     })
 
-    const navigate = index => {
+    const navigate = computable(index => {
         window.location.hash = `#${index}`
         prev(active())
+        if(index >= slides().length) {
+            window.location.hash = `#0`
+            return
+        }
+        if(index < 0) {
+            window.location.hash = `#${slides().length-1}`
+            return
+        }
         return active(index)
+    })
+
+    const toggleFullscreen = () => {
+        const d = document.body,
+            isFullscreen = (document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement)
+
+        if(!isFullscreen){
+            d.requestFullscreen && d.requestFullscreen()
+            d.mozRequestFullScreen && d.mozRequestFullScreen()
+            d.webkitRequestFullScreen && d.webkitRequestFullScreen()
+            d.msRequestFullscreen && d.msRequestFullscreen()
+        } else {
+            document.exitFullscreen && document.exitFullscreen()
+            document.mozCancelFullScreen && document.mozCancelFullScreen()
+            document.webkitExitFullscreen && document.webkitExitFullscreen()
+            document.msExitFullscreen && document.msExitFullscreen()
+        }
     }
 
-    const LEFT = 37,
-        RIGHT = 39
+    const keymap = {
+            37: 'LEFT',
+            39: 'RIGHT',
+            224: 'CMD',
+            17: 'CTRL',
+            70: 'F'
+        },
+        pressed = {}
 
     const events = {
-        onkeyup: (e) => {
+        keydown: (e) => {
             let {keyCode} = e
+            pressed[keymap[keyCode]] = true
 
-            if(LEFT === keyCode) {
+            if(pressed.LEFT) {
                 let next = active()-1
                 if(next < 0) next = slides().length - 1
                 navigate(next)
-            } else if(RIGHT === keyCode) {
+            } else if(pressed.RIGHT) {
                 let next = active()+1
                 if(next > slides().length - 1) next = 0
                 navigate(next)
+            } else if(pressed.CTRL && pressed.F){
+                toggleFullscreen()
+                e.preventDefault()
+                e.stopImmediatePropagation()
             }
+        },
+        keyup: (e) => {
+            let {keyCode} = e
+            pressed[keymap[keyCode]] = false
         }
+    }
+
+    const initEvents = () => {
+        Object.keys(events).forEach(e =>
+                window.addEventListener(e, events[e]))
+        hashChanger()
     }
 
     const config = function(element, init, vdom) {
         if(active() === prev()) return
     }
 
-    const hashChanger = () => window.addEventListener('hashchange', () => {
-        const hash = window.location.hash
-        let slide = parseInt(hash.slice(1))
-        if(slide !== NaN){
-            navigate(slide)
-        }
-    })
+    const hashChanger = () =>
+        window.addEventListener('hashchange', () => {
+            const hash = window.location.hash
+            let slide = parseInt(hash.slice(1))
+            if(slide !== NaN && slide !== active()){
+                navigate(slide)
+            }
+        })
 
     const valueOf = (a) => a()
+
+    const arrows = () => [
+        m('.arrow.left', {onclick: () => navigate(active()-1)}),
+        m('.arrow.right', {onclick: () => navigate(active()+1)})
+    ]
 
     const view = (ctrl) => {
         let a = active(),
@@ -87,19 +134,18 @@ const engine = () => {
 
         return m('html', {config}, [
             m('head', [
-                m('title', 'something'),
+                m('title', `slide: ${active()}`),
                 m('meta', {name:'viewport', content:"width=device-width, initial-scale=1.0"}),
                 m('link', {href: './style.css', type:'text/css', rel:'stylesheet'})
             ]),
-            m('body', events, [
-                m('.slides', _slide)
+            m('body', [
+                m('.slides', _slide),
+                arrows()
             ])
         ])
     }
 
-    const render = (..._) => (hashChanger(), m.mount(qs(..._), {view}))
+    const render = (..._) => (initEvents(), m.mount(qs(..._), {view}))
 
     return { slides, insert, remove, navigate, render }
 }
-
-export {engine, resolver, container, m}
